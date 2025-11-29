6b5a4d0b7057d3e676686b52a582394b88918bbc
ICE <152382318+CIS-Ahmad@users.noreply.github.com>
2025-11-29 21:06:12 +0300

    Update DDL_bronze.sql

--- scripts/bronze layer/DDL_bronze.sql
+++ scripts/bronze layer/DDL_bronze.sql
@@ -14,64 +14,72 @@ schema and reload all data from the beginning.
 ===========================================================
 */
 
-IF OBJECT_ID ('Bronze.crm_cust_info' , 'U') IS NOT NULL 
-    DROP TABLE Bronze.crm_cust_info;
-CREATE TABLE Bronze.crm_cust_info (
+
+IF OBJECT_ID ('Silver.crm_cust_info' , 'U') IS NOT NULL 
+    DROP TABLE Silver.crm_cust_info;
+CREATE TABLE Silver.crm_cust_info (
     cst_id INT,
     cst_key NVARCHAR(50),
     cst_firstName NVARCHAR(50),
     cst_lastName NVARCHAR(50),
     cst_meterial_status NVARCHAR(50),
-    cst_gendr NVARCHAR(1),
-    cst_create_date DATE
+    cst_gendr NVARCHAR(10),
+    cst_create_date DATE,
+	dwh_create_date DAtetime2 default GEtDate()
 );
 
-IF OBJECT_ID ('Bronze.crm_prd_info' , 'U') IS NOT NULL 
-    DROP TABLE Bronze.crm_prd_info;
-CREATE TABLE Bronze.crm_prd_info (
+IF OBJECT_ID ('Silver.crm_prd_info' , 'U') IS NOT NULL 
+    DROP TABLE Silver.crm_prd_info;
+CREATE TABLE Silver.crm_prd_info (
     prd_id INT,
     prd_key NVARCHAR(50),
+	cat_id NVARCHAR(50),
     prd_nm NVARCHAR(50),
     prd_cost INT,
     prd_line NVARCHAR(50),
-    prd_start_dt DATETIME,
-    prd_end_dt DATETIME
+    prd_start_dt Date,
+    prd_end_dt DATE,
+	dwh_create_date DAtetime2 default GEtDate()
 );
 
-IF OBJECT_ID ('Bronze.crm_sales_details' , 'U') IS NOT NULL 
-    DROP TABLE Bronze.crm_sales_details;
-CREATE TABLE Bronze.crm_sales_details (
+IF OBJECT_ID ('Silver.crm_sales_details' , 'U') IS NOT NULL 
+    DROP TABLE Silver.crm_sales_details;
+CREATE TABLE Silver.crm_sales_details (
     sls_ord_num NVARCHAR(50),
     sls_prd_key NVARCHAR(50),
     sls_cust_id INT,
-    sls_order_dt INT,
-    sls_ship_dt INT,
-    sls_due_dt INT,
+    sls_order_dt date,
+    sls_ship_dt date,
+    sls_due_dt date,
     sls_sales INT,
     sls_quantit INT,
-    sls_price INT
+    sls_price INT,
+	dwh_create_date DAtetime2 default GEtDate()
 );
 
-IF OBJECT_ID ('Bronze.erp_loc_a101' , 'U') IS NOT NULL 
-    DROP TABLE Bronze.erp_loc_a101;
-CREATE TABLE Bronze.erp_loc_a101 (
+IF OBJECT_ID ('Silver.erp_loc_a101' , 'U') IS NOT NULL 
+    DROP TABLE Silver.erp_loc_a101;
+CREATE TABLE Silver.erp_loc_a101 (
     cid NVARCHAR(50),
-    cntry NVARCHAR(50)
+    cntry NVARCHAR(50),
+	dwh_create_date DAtetime2 default GEtDate()
 );
 
-IF OBJECT_ID ('Bronze.erp_cust_az12' , 'U') IS NOT NULL 
-    DROP TABLE Bronze.erp_cust_az12;
-CREATE TABLE Bronze.erp_cust_az12 (
+IF OBJECT_ID ('Silver.erp_cust_az12' , 'U') IS NOT NULL 
+    DROP TABLE Silver.erp_cust_az12;
+CREATE TABLE Silver.erp_cust_az12 (
     cid NVARCHAR(50),
     bdate DATE,
-    gen NVARCHAR(50)
+    gen NVARCHAR(50),
+	dwh_create_date DAtetime2 default GEtDate()
 );
 
-IF OBJECT_ID ('Bronze.erp_px_cat_g1v2' , 'U') IS NOT NULL 
-    DROP TABLE Bronze.erp_px_cat_g1v2;
-CREATE TABLE Bronze.erp_px_cat_g1v2 (
+IF OBJECT_ID ('Silver.erp_px_cat_g1v2' , 'U') IS NOT NULL 
+    DROP TABLE Silver.erp_px_cat_g1v2;
+CREATE TABLE Silver.erp_px_cat_g1v2 (
     id NVARCHAR(50),
     cat NVARCHAR(50),
     subcat NVARCHAR(50),
-    maintenance NVARCHAR(50)
+    maintenance NVARCHAR(50),
+	dwh_create_date DAtetime2 default GEtDate()
 );
